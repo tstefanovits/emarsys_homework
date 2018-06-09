cd3d193c4bf14a5213d57976eb9b015e744c7c73
Tamás Stefanovits <tamas.stefanovits@gmail.com>
2018-06-09 20:45:55 +0200

    func lezarva

--- test/test.js
+++ test/test.js
@@ -6,5 +6,6 @@ define([
     function assert(value, description) {
         const result = value ? "pass" : "fail";
         const p = document.createElement("li");
-        p.innerText=`${result} - ${description}`;
-        testDiv.appendChild(p)
+        p.innerText = `${result} - ${description}`;
+        testDiv.appendChild(p);
+    }
